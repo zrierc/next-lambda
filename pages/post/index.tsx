4b84771203740705e5e5d8d14b9c7aff0413c778
zrierc <68382685+zrierc@users.noreply.github.com>
2022-09-09 20:51:12 +0700

    create default post page

---
+++ pages/post/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import styles from '../../styles/Posts.module.css';
+
+const Posts: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h2 className={styles.title}>Hello from main Post page👋</h2>
+
+        <p className={styles.description}>
+          Add prefix to this page to see if dynamic routing is work 👀✨. <br />
+          example:
+          <code className={styles.code}>post/{`{your-prefix}`}</code>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;

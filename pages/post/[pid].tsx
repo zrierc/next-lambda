1b105c3fd3b419bbcfd0da225d5bf2a354061345
zrierc <68382685+zrierc@users.noreply.github.com>
2022-09-09 20:51:46 +0700

    create dynamic page

---
+++ pages/post/[pid].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import styles from '../../styles/Posts.module.css';
+
+const Post = () => {
+  const router = useRouter();
+  const { pid } = router.query;
+
+  return (
+    <div className={styles.main}>
+      <h3 className={styles.description}>Hello from Post Page number {pid}</h3>
+    </div>
+  );
+};
+
+export default Post;
